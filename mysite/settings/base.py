10291805bccaf2cfda373e86bdb1e1a6c3f8ecf9
leelum1 <kevanleelum@gmail.com>
2020-07-28 15:03:46 -0500

    custom storages

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -138,6 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static'),
+]
 
 LOGOUT_REDIRECT_URL = 'index'
